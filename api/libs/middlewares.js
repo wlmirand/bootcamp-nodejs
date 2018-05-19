8895b88731279e838262f917bbc635d48faa1ea4
William Miranda <wlmirand@gmail.com>
2018-05-19 15:13:53 -0300

    Implementando os endpoints para usar o DB
    Signed-off-by: William Miranda <wlmirand@gmail.com>

--- api/libs/middlewares.js
+++ api/libs/middlewares.js
@@ -1,3 +1,13 @@
+/**
+ * Modulo que define os interceptadores
+ **/
+const bodyParser = require('body-parser')
+
 module.exports = (app) => {
     app.set('port', 8080);
+    app.use(bodyParser.json());
+    app.use((req, res, next) => {//remove qualquer id passado no body, pra nao cagar os insert/updates, que sao mantidos pelo banco
+        delete req.body.id;
+        next();
+    });
 }
--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -2,9 +2,72 @@
 //no caso, ele passa para nos a instancia do express.... na variavel app
 module.exports = (app) => {
     
-    app.get('/tasks', (req, res) => {
-        res.json({
-            status: 'Teste porra'
+    //Usamos a estrutura que retornamos no db.js para acessar os modelos do Sequelize
+    const Tasks = app.libs.db.models.Tasks;
+
+    /*Como temos o mesmo endpoint para GET/POST/... podemos usar route para nao repetir
+        app.get  app.put no mesmo endpoint */
+    app.route('/tasks')
+        //remove qualquer id passado no body, pra nao cagar os insert/updates, que sao mantidos pelo banco
+        /*
+        .all((req, res, next) => {
+            delete req.body.id;
+            next();
+        })
+        */
+        .get((req, res) => {
+            Tasks.findAll()
+                .then( (result) => {
+                    res.json(result);
+                })
+                .catch( (error) => {
+                    res.status(500).json(error);
+                })
+        })
+        .post((req, res) => {
+            Tasks.create(req.body)
+                .then( (result) => {
+                    res.json(result);
+                })
+                .catch(error => {
+                    res.status(500).json(error);
+                });
         });
-    });
+
+    app.route('/tasks/:id')
+        .get((req, res) => {
+            Tasks.findById(req.params.id)
+                .then(result => {
+                    res.json(result);
+                })
+                .catch(error => {
+                    res.status(500).json(error);
+                });
+        })
+        .put((req, res) => {
+            Tasks.update(req.body, {
+                where: {
+                    id: req.params.id
+                }
+            })
+            .then(result => {
+                res.json(result);
+            })
+            .catch(error => {
+                res.status(500).json(error);
+            });
+        })
+        .delete((req, res) => {
+            Tasks.destroy({
+                where: {
+                    id: req.params.id
+                }
+            })
+            .then(result => {
+                res.json(result);
+            })
+            .catch(error => {
+                res.status(500).json(error);
+            });
+        });
 }
